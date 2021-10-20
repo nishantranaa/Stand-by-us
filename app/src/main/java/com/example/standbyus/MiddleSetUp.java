8a6df399080c744f5da7487028fd96c4f0851adb
Davide Patelli <mediadavide@gmail.com>
2021-10-20 12:45:27 +1000

    Middle

--- app/src/main/java/com/example/standbyus/MiddleSetUp.java
+++ app/src/main/java/com/example/standbyus/MiddleSetUp.java
@@ -6,20 +6,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MiddleSetUp extends AppCompatActivity {
+    public static String myApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_middle_set_up);
 
-        Button startJuniorSetUp = findViewById(R.id.startMiddle);
-        Intent intentJuniorSetUp = new Intent(this, Middle.class);
-        startJuniorSetUp.setOnClickListener(new View.OnClickListener() {
+        /* Messages */
+        Toast toastNoItemSelected = Toast.makeText(this, "Please select an APP", Toast.LENGTH_SHORT);
+
+        Button startMiddle = findViewById(R.id.startMiddle);
+        Spinner spinnerSeniorSetUpApps = findViewById(R.id.spinnerSeniorSetUpApps);
+
+        Intent intentMiddle = new Intent(this, Middle.class);
+
+        // When you press START BUTTON on MIDDLE ADVANCE SETTINGS
+        startMiddle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(intentJuniorSetUp);
+                int x = spinnerSeniorSetUpApps.getSelectedItemPosition();
+
+                if(x==0){
+                    toastNoItemSelected.show();
+                } else {
+                    switch (x) {
+                        case 1:
+                            myApp = spinnerSeniorSetUpApps.getItemAtPosition(x).toString();
+                            break;
+                        case 2:
+                            myApp = spinnerSeniorSetUpApps.getItemAtPosition(x).toString();
+                            break;
+                        case 3:
+                            myApp = spinnerSeniorSetUpApps.getItemAtPosition(x).toString();
+                            break;
+                        case 4:
+                            myApp = spinnerSeniorSetUpApps.getItemAtPosition(x).toString();
+                            break;
+                        case 5:
+                            myApp = spinnerSeniorSetUpApps.getItemAtPosition(x).toString();
+                            break;
+                        case 6:
+                            myApp = spinnerSeniorSetUpApps.getItemAtPosition(x).toString();
+                            break;
+                    }
+                    if(x!=0) {
+                        MainActivity.start = true;
+                        MainActivity.middle = true;
+                        startActivity(intentMiddle);
+                    }
+                }
             }
         });
     }
