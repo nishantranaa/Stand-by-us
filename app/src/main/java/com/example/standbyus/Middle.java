4a26a8b3cf1c53611d92636f6d5641ed3c329242
Davide Patelli <mediadavide@gmail.com>
2021-10-20 12:45:18 +1000

    Middle

--- app/src/main/java/com/example/standbyus/Middle.java
+++ app/src/main/java/com/example/standbyus/Middle.java
@@ -2,7 +2,12 @@ package com.example.standbyus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Middle extends AppCompatActivity {
 
@@ -10,5 +15,7 @@ public class Middle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_middle);
+
+
     }
 }
