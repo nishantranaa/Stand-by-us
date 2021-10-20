e6cb1ae7d6737593152a6840d10ade0edfa1ac08
Davide Patelli <mediadavide@gmail.com>
2021-10-20 16:07:17 +1000

    First iteration

--- app/src/main/java/com/example/standbyus/Junior.java
+++ app/src/main/java/com/example/standbyus/Junior.java
@@ -1,8 +1,16 @@
 package com.example.standbyus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class Junior extends AppCompatActivity {
 
@@ -10,5 +18,35 @@ public class Junior extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_junior);
+
+
+        TextView juniorView = findViewById(R.id.juniorView);
+        juniorView.setText(JuniorSetUp.userName.toUpperCase());
+
+        Spinner spinnerJuniorApps = findViewById(R.id.spinnerJuniorSetUpApps);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.settings) {
+            Intent intentSetting = new Intent(this, SetUp.class);
+            startActivity(intentSetting);
+            return true;
+        } else {
+            MainActivity.start = false;
+            MainActivity.junior = false;
+            MainActivity.middle = false;
+            MainActivity.senior = false;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
