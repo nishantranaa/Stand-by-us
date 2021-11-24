e9b4b379a3ac98a679c87e8280e4eff21f54676d
Davide Patelli <mediadavide@gmail.com>
2021-11-24 11:11:35 +1000

    senior makes a call done!

--- app/src/main/java/com/example/standbyus/SeniorSetUp.java
+++ app/src/main/java/com/example/standbyus/SeniorSetUp.java
@@ -2,13 +2,18 @@ package com.example.standbyus;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SeniorSetUp extends AppCompatActivity {
@@ -23,6 +28,14 @@ public class SeniorSetUp extends AppCompatActivity {
     private EditText firstContactPhoneNumber;
     private EditText secondContactPhoneNumber;
     private EditText thirdContactPhoneNumber;
+    private Button buttonFindImage1;
+    private ImageButton showImage1;
+    private Button buttonFindImage2;
+    private ImageButton showImage2;
+    private Button buttonFindImage3;
+    private ImageButton showImage3;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
 
     //private static int numOfContacts =0;
     //public static ArrayList<String> myContacts;
@@ -33,7 +46,7 @@ public class SeniorSetUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senior_set_up);
 
-        //myContacts = new ArrayList<>();
+
         startSenior = findViewById(R.id.startJunior);
         seniorSetUpView = findViewById(R.id.juniorSetUpView);
         showNamePerson = findViewById(R.id.showNamePersonSenior);
@@ -51,6 +64,19 @@ public class SeniorSetUp extends AppCompatActivity {
         showNamePerson.setText(mainPreferences.getString("name", "").toUpperCase());
         /* Senior preferences */
         sharedPreferences = getApplicationContext().getSharedPreferences("seniorPreferences", MODE_PRIVATE);
+        /* get the images */
+        buttonFindImage1 = findViewById(R.id.buttonFindImage1);
+        buttonFindImage2 = findViewById(R.id.buttonFindImage2);
+        buttonFindImage3 = findViewById(R.id.buttonFindImage3);
+        showImage1 = findViewById(R.id.showImage1);
+        showImage2 = findViewById(R.id.showImage2);
+        showImage3 = findViewById(R.id.showImage3);
+        buttonFindImage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery1(); // Open images gallery
+            }
+        });
         /* When you press START BUTTON on SENIOR ADVANCE SETTINGS */
         startSenior.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,4 +95,41 @@ public class SeniorSetUp extends AppCompatActivity {
             }
         });
     }
+
+    private void openGallery1() {
+        /* open the images gallery on the phone */
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+        //return true;
+    }
+    private void openGallery2() {
+        /* open the images gallery on the phone */
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+        //return true;
+    }
+    private void openGallery3() {
+        /* open the images gallery on the phone */
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+        //return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i(">>>>>>>>>>>>", String.valueOf(requestCode));
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            //showImage1.setImageURI(imageUri);
+        }/*
+        if (resultCode == 0){
+            showImage1.setImageURI(imageUri);
+        } else if (requestCode == 1) {
+            showImage2.setImageURI(imageUri);
+        } else if (requestCode == 2) {
+            showImage3.setImageURI(imageUri);
+        }*/
+    }
+
 }
